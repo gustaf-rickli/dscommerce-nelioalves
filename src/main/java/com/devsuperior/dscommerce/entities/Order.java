2ffab02be7dfe7e9d73d2e7c9987bd6b247a4516
Gustavo Rickli Agibert Aragão <gustavorickli256@gmail.com>
2024-12-03 15:14:54 -0300

    orderItem, orderItemPk, method's getOrders in Product and getProducts in Order

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -3,7 +3,10 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 
 @Entity
@@ -24,6 +27,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() { }
 
@@ -65,6 +71,14 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return  items.stream().map(e -> e.getProduct()).toList();
     }
 
     @Override
